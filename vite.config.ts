44209156ca13db8bc7434f4bb06a4bcac4040949
g-vidhulakripali <g.vidhulakripali@gmail.com>
2026-03-21 23:30:28 +0100

    Deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listens on all local IPs (0.0.0.0)
+  },
+  preview: {
+    host: true, // Same for preview server
+  }
 })
+
